f7d0f72a88c0fcfd249cd90b5459b35442f3e57c
Flame787898 <a.uskov@dev-branch.com>
2022-12-15 16:43:59 +0200

    #5666 created regions and placed there content blocks for home page

---
+++ web/themes/custom/flame/gulp/config/path.js
@@ -0,0 +1,14 @@
+const buildFolder = `../flame/dist`
+const srcFolder = `../flame`
+
+export const path = {
+  build:{
+    css:`${buildFolder}/css`
+  },
+  src:{
+    scss:`${srcFolder}/assets/sass/style.scss`
+  },
+  watch:{
+    scss:`${srcFolder}/assets/sass/**/*.scss`
+  }
+}
