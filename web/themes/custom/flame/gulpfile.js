25f05397e346fb8a711e77d2eeedecd749be26ad
Flame787898 <a.uskov@dev-branch.com>
2022-12-20 10:08:00 +0200

    #5720 Refactored code(css ,js)

--- web/themes/custom/flame/gulpfile.js
+++ web/themes/custom/flame/gulpfile.js
@@ -4,17 +4,16 @@ import {scss} from './gulp/task/scss.js'
 import {server} from './gulp/task/server.js'
 import {plugins} from './gulp/config/plugins.js';
 import reload from "gulp-livereload";
+
 global.app = {
-  path,
-  gulp,
-  plugins
+  path, gulp, plugins
 }
 
-function watcher () {
+function watcher() {
   reload.listen();
-  gulp.watch(path.watch.scss,scss)
+  gulp.watch(path.watch.scss, scss)
 }
 
 const mainTask = gulp.parallel(scss)
-const dev = gulp.series(mainTask,gulp.parallel(watcher,server))
-gulp.task('default',dev)
+const dev = gulp.series(mainTask, gulp.parallel(watcher, server))
+gulp.task('default', dev)
