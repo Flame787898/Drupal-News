88d9baa8a05e79e02aa8a128bfb46f05e7f6ce3a
Flame787898 <a.uskov@dev-branch.com>
2023-01-03 18:31:11 +0200

    #5797 Fix bugs after merge request (css and js)

--- web/themes/custom/flame/assets/js/secondStickySidebar.js
+++ web/themes/custom/flame/assets/js/secondStickySidebar.js
@@ -4,43 +4,45 @@ Drupal.behaviors.secondStickySidebarsBehavior = {
         const rightFirstSidebar = document.querySelector('.second__news__right-sidebar')
         const newsList = document.querySelector('.second__news__list')
         const newsSection = document.querySelector('.second__news')
-        document.addEventListener("scroll", (event) => {
-            const scrollY = window.pageYOffset
-            const beginSticky = 3410
-            const endRightSticky = 4080
-            const endLeftSticky = 4300
+        if (leftFirstSidebar && window.innerWidth > 1400) {
+            document.addEventListener("scroll", (event) => {
+                const scrollY = window.pageYOffset
+                const beginSticky = 3510
+                const endRightSticky = 4115
+                const endLeftSticky = 4400
 
-            if (scrollY > beginSticky) {
-                leftFirstSidebar.style.position = 'fixed'
-                rightFirstSidebar.style.position = 'fixed'
-                leftFirstSidebar.style.top = '45px'
-                rightFirstSidebar.style.top = '45px'
-                rightFirstSidebar.style.right = '260px'
-                newsList.style.marginLeft = "280px"
-                newsSection.style.marginTop = "30px"
-                newsSection.style.marginBottom = "220px"
-            } else if (scrollY < beginSticky) {
-                leftFirstSidebar.style.position = 'relative'
-                leftFirstSidebar.style.top = '0px'
-                newsList.style.marginLeft = "0px"
-                rightFirstSidebar.style.position = 'relative'
-                rightFirstSidebar.style.right = '0px'
-                rightFirstSidebar.style.top = '0px'
-                newsSection.style.marginBottom = "0px"
-                newsSection.style.marginTop = "0px"
-            }
-            if (scrollY > endRightSticky) {
-                rightFirstSidebar.style.position = 'relative'
-                rightFirstSidebar.style.top = `${endRightSticky-beginSticky}px`
-                rightFirstSidebar.style.right = '0px'
-              // newsSection.style.marginBottom = "100px"
-            }
-            if (scrollY > endLeftSticky) {
-                leftFirstSidebar.style.position = 'relative'
-                leftFirstSidebar.style.top = `${endLeftSticky - beginSticky}px`
-                newsList.style.marginLeft = "0px"
-            }
-        });
+                if (scrollY > beginSticky) {
+                    leftFirstSidebar.style.position = 'fixed'
+                    rightFirstSidebar.style.position = 'fixed'
+                    leftFirstSidebar.style.top = '45px'
+                    rightFirstSidebar.style.top = '45px'
+                    rightFirstSidebar.style.right = '260px'
+                    newsList.style.marginLeft = "280px"
+                    newsSection.style.marginTop = "30px"
+                    newsSection.style.marginBottom = "220px"
+                } else if (scrollY < beginSticky) {
+                    leftFirstSidebar.style.position = 'relative'
+                    leftFirstSidebar.style.top = '0px'
+                    newsList.style.marginLeft = "0px"
+                    rightFirstSidebar.style.position = 'relative'
+                    rightFirstSidebar.style.right = '0px'
+                    rightFirstSidebar.style.top = '0px'
+                    newsSection.style.marginBottom = "0px"
+                    newsSection.style.marginTop = "0px"
+                }
+                if (scrollY > endRightSticky) {
+                    rightFirstSidebar.style.position = 'relative'
+                    rightFirstSidebar.style.top = `${endRightSticky - beginSticky}px`
+                    rightFirstSidebar.style.right = '0px'
+                  // newsSection.style.marginBottom = "100px"
+                }
+                if (scrollY > endLeftSticky) {
+                    leftFirstSidebar.style.position = 'relative'
+                    leftFirstSidebar.style.top = `${endLeftSticky - beginSticky}px`
+                    newsList.style.marginLeft = "0px"
+                }
+            });
+        }
     }
 };
 
