39cf58f44fc40780b11e0b04c8ead7f5863d2bc8
Flame787898 <a.uskov@dev-branch.com>
2023-01-11 16:32:15 +0200

    #5840 Created exchange rates plugin

---
+++ web/modules/custom/ exchange_rates/src/Plugin/Block/ExchangeRatesBlock.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\exchange_rates\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Displays block
+ *
+ * @Block(
+ *   id = "exchange_rates_block",
+ *   admin_label = "Exchange rates"
+ * )
+ */
+class ExchangeRatesBlock extends BlockBase {
+
+  /**
+   * Get all exchange rate from api
+   *
+   * @param $url
+   *
+   * @return mixed
+   */
+  function get_api($url): mixed {
+    try {
+      $client = \Drupal::httpClient();
+      $get_request = $client->get($url);
+    } catch (\Exception $error) {
+      $logger = \Drupal::logger('HTTP Client error');
+      $logger->error($error->getMessage());
+    }
+    $data = json_decode($get_request->getBody());
+    return $data;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json';
+    $data = $this->get_api($url);
+    return [
+      '#theme' => 'exchange-block',
+      '#data' => $data,
+    ];
+
+  }
+
+}
