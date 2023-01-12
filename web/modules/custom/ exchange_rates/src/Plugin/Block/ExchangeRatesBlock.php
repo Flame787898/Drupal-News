046d5be93e58d3a94b2858705992d59e985e7195
Flame787898 <a.uskov@dev-branch.com>
2023-01-12 15:17:06 +0200

    #5840-3 Created a service and move the main functionality (requests sending and results processing) into it.

--- web/modules/custom/ exchange_rates/src/Plugin/Block/ExchangeRatesBlock.php
+++ web/modules/custom/ exchange_rates/src/Plugin/Block/ExchangeRatesBlock.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\exchange_rates\Plugin\Block;
+
 use Drupal\Core\Block\BlockBase;
 
 /**
@@ -14,39 +15,21 @@ use Drupal\Core\Block\BlockBase;
 class ExchangeRatesBlock extends BlockBase {
 
   /**
-   * Get all exchange rate from api
-   *
-   * @param $url
-   *
-   * @return mixed
-   */
-  function get_api($url): mixed {
-    try {
-      $client = \Drupal::httpClient();
-      $get_request = $client->get($url);
-    } catch (\Exception $error) {
-      $logger = \Drupal::logger('HTTP Client error');
-      $logger->error($error->getMessage());
-    }
-    $data = json_decode($get_request->getBody());
-    return $data;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function build() {
-    $api_url = \Drupal::config('exchange_rates.settings')->get('api_base_url');
-    $allow_request = \Drupal::config('exchange_rates.settings')->get('disabled_api');
-    if($allow_request == 0){
-      $data = $this->get_api($api_url);
+    $exchange_api_connector_service = \Drupal::service('exchange_rates.api_connector');
+
+    if ($exchange_api_connector_service->allowRequest() == 0) {
+      $data = $exchange_api_connector_service->getExchangeRates();
     }
-    else{
+    else {
       return [
         '#theme' => 'exchange-block',
-        '#data' => ' ',
+        '#data' => '',
       ];
     }
+
     return [
       '#theme' => 'exchange-block',
       '#data' => $data,
